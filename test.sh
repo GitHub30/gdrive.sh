19e5c94dfbbfcd6dca3ad820a7e6315d97a6ff08
Zapus <funaox@gmail.com>
2018-08-29 01:45:03 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+set -xe
+
+bash gdrive.sh 0B4y35FiV1wh7QVR6VXJ5dWExSTQ
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/open?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7QVR6VXJ5dWExSTQ/view?usp=sharing
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7QVR6VXJ5dWExSTQ/view
+rm CRF++-0.58.tar.gz
+bash gdrive.sh 0B4y35FiV1wh7SDd1Q1dUQkZQaUU
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/open?id=0B4y35FiV1wh7SDd1Q1dUQkZQaUU
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7SDd1Q1dUQkZQaUU/view?usp=sharing
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7SDd1Q1dUQkZQaUU/view
+rm cabocha-0.69.tar.bz2
+
+set +xe
