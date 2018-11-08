77bf14bc7d34d2b1d25598fd1812a92b13ee8114
Zapus <funaox@gmail.com>
2018-11-08 15:06:11 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -19,8 +19,8 @@ bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7SDd1Q1dUQkZQaUU/view
 rm cabocha-0.69.tar.bz2
 bash gdrive.sh https://docs.google.com/file/d/0BwmPMFurnk9Pak5zWEVyOUZESms/edit
 rm Ubuntu-12.04-LTS_x86_64.iso
-sh gdrive.sh https://drive.google.com/open?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
-rm img_align_celeba.zip
+#sh gdrive.sh https://drive.google.com/open?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
+#rm img_align_celeba.zip
 sh gdrive.sh 1TdFtOzKlGQnckOavxlwgJuW4nc9oGU6U
 rm -rf yt/
 sh gdrive.sh https://drive.google.com/drive/folders/0B7EVK8r0v71pOC0wOVZlQnFfaGs
